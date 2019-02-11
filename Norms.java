47c734767ae817f73425b67ac511c19c614be365
Krishna Kurtakoti <30724917+krishnakurtakoti@users.noreply.github.com>
2019-02-11 14:16:48 +0530

    Created Norms.java
    This is the model we are using.

---
+++ Norms.java
@@ -0,0 +1,11 @@
+public class Norms {
+    public String normTitle;
+    public String normDescription;
+    public  String url;
+
+    public Norms(String normTitle, String normDescription, String url){
+        this.normDescription = normDescription;
+        this.normTitle = normTitle;
+        this.url = url;
+    }
+}
